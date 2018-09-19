43f41a8abaea2e7f671ed005da84ba8d5a9c4027
Ville Tolvanen <ville.tolvanen@indooratlas.com>
2018-09-19 11:05:38 +0300

    Added new example demonstrating lockFloor and lockIndoors APIs

---
+++ Basic/src/main/java/com/indooratlas/android/sdk/examples/lockfloor/LockFloorActivity.java
@@ -0,0 +1,269 @@
+package com.indooratlas.android.sdk.examples.lockfloor;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.examples.R;
+import com.indooratlas.android.sdk.examples.SdkExample;
+import com.indooratlas.android.sdk.examples.utils.ExampleUtils;
+
+import java.util.Locale;
+
+/**
+ * Simple example that demonstrates basic interaction with {@link IALocationManager}.
+ */
+@SdkExample(description = R.string.example_lockfloor_description)
+public class LockFloorActivity extends AppCompatActivity
+        implements IALocationListener, IARegion.Listener {
+
+    static final String FASTEST_INTERVAL = "fastestInterval";
+    static final String SHORTEST_DISPLACEMENT = "shortestDisplacement";
+
+    private IALocationManager mLocationManager;
+    private TextView mLog;
+    private ScrollView mScrollView;
+    private long mRequestStartTime;
+
+    private long mFastestInterval = -1L;
+    private float mShortestDisplacement = -1f;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            mFastestInterval = savedInstanceState.getLong(FASTEST_INTERVAL);
+            mShortestDisplacement = savedInstanceState.getFloat(SHORTEST_DISPLACEMENT);
+        }
+
+        setContentView(R.layout.activity_lockfloor);
+        mLog = (TextView) findViewById(R.id.text);
+        mScrollView = (ScrollView) findViewById(R.id.scroller);
+        mLocationManager = IALocationManager.create(this);
+
+        // Register long click for sharing traceId
+        ExampleUtils.shareTraceId(mLog, LockFloorActivity.this, mLocationManager);
+
+        mRequestStartTime = SystemClock.elapsedRealtime();
+        IALocationRequest request = IALocationRequest.create();
+
+        mLocationManager.requestLocationUpdates(request, LockFloorActivity.this);
+
+        log("requestLocationUpdates");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationManager.destroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mLocationManager.registerRegionListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationManager.unregisterRegionListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_simple, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int id = item.getItemId();
+        switch (id) {
+            case R.id.action_clear:
+                mLog.setText(null);
+                return true;
+            case R.id.action_share:
+                shareLog();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void lockFloor(View view) {
+        askFloor();
+    }
+
+    @Override
+    public void onLocationChanged(IALocation location) {
+        log(String.format(Locale.US, "%f,%f, accuracy: %.2f, certainty: %.2f",
+                location.getLatitude(), location.getLongitude(), location.getAccuracy(),
+                location.getFloorCertainty()));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case IALocationManager.STATUS_CALIBRATION_CHANGED:
+                String quality = "unknown";
+                switch (extras.getInt("quality")) {
+                    case IALocationManager.CALIBRATION_POOR:
+                        quality = "Poor";
+                        break;
+                    case IALocationManager.CALIBRATION_GOOD:
+                        quality = "Good";
+                        break;
+                    case IALocationManager.CALIBRATION_EXCELLENT:
+                        quality = "Excellent";
+                        break;
+                }
+                log("Calibration change. Quality: " + quality);
+                break;
+            case IALocationManager.STATUS_AVAILABLE:
+                log("onStatusChanged: Available");
+                break;
+            case IALocationManager.STATUS_LIMITED:
+                log("onStatusChanged: Limited");
+                break;
+            case IALocationManager.STATUS_OUT_OF_SERVICE:
+                log("onStatusChanged: Out of service");
+                break;
+            case IALocationManager.STATUS_TEMPORARILY_UNAVAILABLE:
+                log("onStatusChanged: Temporarily unavailable");
+        }
+    }
+
+    @Override
+    public void onEnterRegion(IARegion region) {
+        log("onEnterRegion: " + regionType(region.getType()) + ", " + region.getId());
+    }
+
+    @Override
+    public void onExitRegion(IARegion region) {
+        log("onExitRegion: " + regionType(region.getType()) + ", " + region.getId());
+    }
+
+    /**
+     * Turn {@link IARegion#getType()} to human-readable name
+     */
+    private String regionType(int type) {
+        switch (type) {
+            case IARegion.TYPE_UNKNOWN:
+                return "unknown";
+            case IARegion.TYPE_FLOOR_PLAN:
+                return "floor plan";
+            case IARegion.TYPE_VENUE:
+                return "venue";
+            default:
+                return Integer.toString(type);
+        }
+    }
+
+    /**
+     * Append message into log prefixing with duration since start of location requests.
+     */
+    private void log(String msg) {
+        double duration = mRequestStartTime != 0
+                ? (SystemClock.elapsedRealtime() - mRequestStartTime) / 1e3
+                : 0d;
+        mLog.append(String.format(Locale.US, "\n[%06.2f]: %s", duration, msg));
+        mScrollView.smoothScrollBy(0, mLog.getBottom());
+    }
+
+    private void askFloor() {
+
+        final int minValue = -10;
+        final int maxValue = 10;
+        final NumberPicker numberPicker = new NumberPicker(this);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(maxValue - minValue);
+        numberPicker.setValue(10);
+        numberPicker.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int index) {
+                return Integer.toString(index + minValue);
+            }
+        });
+
+        android.support.v7.app.AlertDialog.Builder builder =
+                new android.support.v7.app.AlertDialog.Builder(this);
+        builder.setTitle("Lock / unlock floor");
+        builder.setMessage("Floor number:");
+        builder.setView(numberPicker);
+        builder.setPositiveButton("Lock floor", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mLocationManager != null) {
+                    int number = numberPicker.getValue() + minValue;
+                    mLocationManager.lockFloor(number);
+                    log("Locking floor " + number);
+                }
+                dialog.cancel();
+            }
+        });
+        builder.setNeutralButton("Cancel", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setNegativeButton("Unlock", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mLocationManager != null) {
+                    mLocationManager.unlockFloor();
+                    log("Unlocking floor");
+                }
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    public void lockIndoors(View view) {
+        if (mLocationManager != null) {
+            mLocationManager.lockIndoors(true);
+            log("Locking indoors");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong(FASTEST_INTERVAL, mFastestInterval);
+        savedInstanceState.putFloat(SHORTEST_DISPLACEMENT, mShortestDisplacement);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /**
+     * Share current log content using registered apps.
+     */
+    private void shareLog() {
+        Intent sendIntent = new Intent()
+                .setAction(Intent.ACTION_SEND)
+                .putExtra(Intent.EXTRA_TEXT, mLog.getText())
+                .setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+}
