36c7b649931ba2819f8093cf1af688e21f828967
Otto Seiskari <otto.seiskari@indooratlas.com>
2018-09-25 10:37:16 +0300

    Further tweaking of the wayfinding example

--- Basic/src/main/java/com/indooratlas/android/sdk/examples/wayfinding/WayfindingOverlayActivity.java
+++ Basic/src/main/java/com/indooratlas/android/sdk/examples/wayfinding/WayfindingOverlayActivity.java
@@ -1,13 +1,9 @@
 package com.indooratlas.android.sdk.examples.wayfinding;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -33,6 +29,8 @@ import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IAOrientationListener;
+import com.indooratlas.android.sdk.IAOrientationRequest;
 import com.indooratlas.android.sdk.IARegion;
 import com.indooratlas.android.sdk.IARoute;
 import com.indooratlas.android.sdk.IAWayfindingListener;
@@ -52,8 +50,7 @@ import java.util.List;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 @SdkExample(description = R.string.example_wayfinding_description)
-public class WayfindingOverlayActivity extends FragmentActivity implements LocationListener,
-        GoogleMap.OnMapClickListener {
+public class WayfindingOverlayActivity extends FragmentActivity implements GoogleMap.OnMapClickListener {
     private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 42;
 
     private static final String TAG = "IndoorAtlasExample";
@@ -68,8 +65,8 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
     private IALocationManager mIALocationManager;
     private Target mLoadTarget;
     private boolean mCameraPositionNeedsUpdating = true; // update on first location
-    private boolean mShowIndoorLocation = false;
     private Marker mDestinationMarker;
+    private Marker mHeadingMarker;
     private List<Polyline> mPolylines = new ArrayList<>();
     private IARoute mCurrentRoute;
 
@@ -78,7 +75,26 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
         @Override
         public void onWayfindingUpdate(IARoute route) {
             mCurrentRoute = route;
+            if (hasArrivedToDestination(route)) {
+                // stop wayfinding
+                showInfo("You're there!");
+                mCurrentRoute = null;
+                mWayfindingDestination = null;
+                mIALocationManager.removeWayfindingUpdates();
+            }
             updateRouteVisualization();
+        }
+    };
+
+    private IAOrientationListener mOrientationListener = new IAOrientationListener() {
+        @Override
+        public void onHeadingChanged(long timestamp, double heading) {
+            updateHeading(heading);
+        }
+
+        @Override
+        public void onOrientationChange(long timestamp, double[] quaternion) {
+            // we do not need full device orientation in this example, just the heading
         }
     };
 
@@ -91,16 +107,28 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
                 mCircle = mMap.addCircle(new CircleOptions()
                         .center(center)
                         .radius(accuracyRadius)
-                        .fillColor(0x801681FB)
-                        .strokeColor(0x800A78DD)
+                        .fillColor(0x201681FB)
+                        .strokeColor(0x500A78DD)
                         .zIndex(1.0f)
                         .visible(true)
                         .strokeWidth(5.0f));
+                mHeadingMarker = mMap.addMarker(new MarkerOptions()
+                        .position(center)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_blue_dot))
+                        .anchor(0.5f, 0.5f)
+                        .flat(true));
             }
         } else {
             // move existing markers position to received location
             mCircle.setCenter(center);
+            mHeadingMarker.setPosition(center);
             mCircle.setRadius(accuracyRadius);
+        }
+    }
+
+    private void updateHeading(double heading) {
+        if (mHeadingMarker != null) {
+            mHeadingMarker.setRotation((float)heading);
         }
     }
 
@@ -131,9 +159,7 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
             }
             mFloor = newFloor;
 
-            if (mShowIndoorLocation) {
-                showLocationCircle(center, location.getAccuracy());
-            }
+            showLocationCircle(center, location.getAccuracy());
 
             // our camera position needs updating if location has significantly changed
             if (mCameraPositionNeedsUpdating) {
@@ -163,66 +189,15 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
                 } else {
                     mGroundOverlay.setTransparency(0.0f);
                 }
-
-                mShowIndoorLocation = true;
-                showInfo("Showing IndoorAtlas SDK\'s location output");
             }
-            showInfo("Enter " + (region.getType() == IARegion.TYPE_VENUE
-                    ? "VENUE "
-                    : "FLOOR_PLAN ") + region.getId());
         }
 
         @Override
         public void onExitRegion(IARegion region) {
-            if (mGroundOverlay != null) {
-                // Indicate we left this floor plan but leave it there for reference
-                // If we enter another floor plan, this one will be removed and another one loaded
-                mGroundOverlay.setTransparency(0.5f);
-            }
-
-            mShowIndoorLocation = false;
-            showInfo("Exit " + (region.getType() == IARegion.TYPE_VENUE
-                    ? "VENUE "
-                    : "FLOOR_PLAN ") + region.getId());
         }
 
     };
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSION_ACCESS_FINE_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startListeningPlatformLocations();
-                }
-                break;
-            }
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (!mShowIndoorLocation) {
-            Log.d(TAG, "new LocationService location received with coordinates: " + location.getLatitude()
-                    + "," + location.getLongitude());
-
-            showLocationCircle(
-                    new LatLng(location.getLatitude(), location.getLongitude()),
-                    location.getAccuracy());
-        }
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -243,8 +218,6 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
             return;
         }
-
-        startListeningPlatformLocations();
     }
 
     @Override
@@ -268,6 +241,10 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
         // start receiving location updates & monitor region changes
         mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mListener);
         mIALocationManager.registerRegionListener(mRegionListener);
+        mIALocationManager.registerOrientationListener(
+                // update if heading changes by 1 degrees or more
+                new IAOrientationRequest(1, 0),
+                mOrientationListener);
 
         if (mWayfindingDestination != null) {
             mIALocationManager.requestWayfindingUpdates(mWayfindingDestination, mWayfindingListener);
@@ -282,6 +259,7 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
         // unregister location & region changes
         mIALocationManager.removeLocationUpdates(mListener);
         mIALocationManager.unregisterRegionListener(mRegionListener);
+        mIALocationManager.unregisterOrientationListener(mOrientationListener);
 
         if (mWayfindingDestination != null) {
             mIALocationManager.removeWayfindingUpdates();
@@ -326,7 +304,9 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
                 public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
                     Log.d(TAG, "onBitmap loaded with dimensions: " + bitmap.getWidth() + "x"
                             + bitmap.getHeight());
-                    setupGroundOverlay(floorPlan, bitmap);
+                    if (mOverlayFloorPlan != null && floorPlan.getId().equals(mOverlayFloorPlan.getId())) {
+                        setupGroundOverlay(floorPlan, bitmap);
+                    }
                 }
 
                 @Override
@@ -368,14 +348,6 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
         snackbar.show();
     }
 
-    private void startListeningPlatformLocations() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager != null && (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-        }
-    }
-
     @Override
     public void onMapClick(LatLng point) {
         if (mMap != null) {
@@ -401,6 +373,10 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
         }
     }
 
+    private boolean hasArrivedToDestination(IARoute route) {
+        return route.getLegs().size() == 1 && route.getLegs().get(0).getLength() < 10.0;
+    }
+
     /**
      * Clear the visualizations for the wayfinding paths
      */
@@ -423,6 +399,17 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
         }
 
         for (IARoute.Leg leg : mCurrentRoute.getLegs()) {
+
+            if (leg.getEdgeIndex() == null) {
+                // Legs without an edge index are, in practice, the last and first legs of the
+                // route. They connect the destination or current location to the routing graph.
+                // All other legs travel along the edges of the routing graph.
+
+                // Omitting these "artificial edges" in visualization can improve the aesthetics
+                // of the route. Alternatively, they could be visualized with dashed lines.
+                continue;
+            }
+
             PolylineOptions opt = new PolylineOptions();
             opt.add(new LatLng(leg.getBegin().getLatitude(), leg.getBegin().getLongitude()));
             opt.add(new LatLng(leg.getEnd().getLatitude(), leg.getEnd().getLongitude()));
@@ -434,6 +421,7 @@ public class WayfindingOverlayActivity extends FragmentActivity implements Locat
             } else {
                 opt.color(0x300000FF);
             }
+
             mPolylines.add(mMap.addPolyline(opt));
         }
     }
